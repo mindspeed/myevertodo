f4222d4d5936837c48b520848588d2c1bf289c68
mindspeed <nodegitrocks@gmail.com>
2013-10-18 23:06:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,10 @@
-/*
-module.exports = {
-		
-	 evernoteConsumerKey: 'nodesumeet'
-	,evernoteConsumerSecret: '1f6c73627eb3a83c'
-	,evernoteUsedSandbox: true
-	
-	,serverUrl:'http://localhost:8081'
-	,serverPort:8081,
-};*/
+
 
 module.exports = {
 
 		
-	 evernoteConsumerKey: 'sumeet14-4162'
-	,evernoteConsumerSecret: '73daf5b83577a99b'
+	 evernoteConsumerKey: 'consumerkey'
+	,evernoteConsumerSecret: 'consumersecret'
 	,evernoteUsedSandbox: false
 	
 	,serverUrl:'http://localhost:8081'
